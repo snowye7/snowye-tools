aa8555f9460ff42f09a6f55389b5e699a21db260
snowye7 <601994226@qq.com>
2024-06-18 16:22:50 +0800

    chore: Update package.json and tsconfig.json
    The package.json file was updated to increase the version number from 0.0.2 to 0.0.3. Additionally, the dayjs dependency was added with a version requirement of ^1.11.11.
    
    The tsconfig.json file was modified to set the target to ESNext and the moduleResolution to Bundler.
    
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,35 @@
-declare global {
-    interface Array<T> {
-        with(this: T[], index: number, value: T): T[]
-        toReversed(this: T[]): T[]
-        toShifted(this: T[]): T[]
-        toPopped(this: T[]): T[]
-        toSorted(this: T[], compareFn?: (a: T, b: T) => number): T[]
-        toDeduplicated(this: T[], compareFn?: (a: T, b: T) => boolean): T[]
-        toSpliced(this: T[], start: number, deleteCount?: number, ...items: T[]): T[]
-        toPushed(this: T[], ...items: T[]): T[]
-        toUnshifted(this: T[], ...items: T[]): T[]
-        toExchange(this: T[], a: number, b: number): T[]
-        at(this: T[], index: number): T | undefined
-    }
+import dayjs from "dayjs"
+
+export function filterObject<T extends Record<string, any>>(props: T): Partial<T> {
+    return Object.keys(props).reduce((prev, item) => {
+        if (props[item]) {
+            return { ...prev, [item]: props[item] }
+        }
+        return { ...prev }
+    }, {})
 }
 
-export function extendArrayPureFunction() {
-    if (!Array.prototype.hasOwnProperty("with")) {
-        class A {
-            static with<T>(this: T[], index: number, value: T): T[] {
-                if (!Number.isInteger(index) || index >= this.length || index < this.length * -1) {
-                    throw new RangeError(`Invalid index : ${index}`)
-                }
-                const $ = [...this]
-                $[index >= 0 ? index : this.length + index] = value
-                return $
-            }
-        }
-        Array.prototype.with = A.with
+export async function requestAll<T = any>(fn: (pageNo: number, pageSize: number) => Promise<{ total: number; list: T[] }>, pageSize: number = 1000) {
+    let resultList: T[] = []
+    const result = await fn(1, pageSize)
+    if (!result) {
+        return []
     }
-    if (!Array.prototype.hasOwnProperty("toReversed")) {
-        const toReversed = function <T>(this: T[]): T[] {
-            const $ = [...this]
-            $.reverse()
-            return $
-        }
-        Array.prototype.toReversed = toReversed
+    resultList = [...(result.list ?? [])]
+    const pages = Math.ceil(result.total / pageSize)
+    if (pages === 1) {
+        return resultList
     }
-    if (!Array.prototype.hasOwnProperty("toShifted")) {
-        const toShifted = function <T>(this: T[]): T[] {
-            const $ = [...this]
-            $.shift()
-            return $
+    for (let i = 2; i <= pages; i++) {
+        const result = await fn(i, pageSize)
+        if (!result) {
+            return []
         }
-        Array.prototype.toShifted = toShifted
+        resultList = [...resultList, ...(result.list ?? [])]
     }
-    if (!Array.prototype.hasOwnProperty("toPopped")) {
-        const toPopped = function <T>(this: T[]): T[] {
-            const $ = [...this]
-            $.pop()
-            return $
-        }
-        Array.prototype.toPopped = toPopped
-    }
-    if (!Array.prototype.hasOwnProperty("toSorted")) {
-        const toSorted = function <T>(this: T[], compareFn?: (a: T, b: T) => number): T[] {
-            const $ = [...this]
-            $.sort(compareFn)
-            return $
-        }
-        Array.prototype.toSorted = toSorted
-    }
-    if (!Array.prototype.hasOwnProperty("toDeduplicated")) {
-        const toDeduplicated = function <T>(this: T[], compareFn?: (a: T, b: T) => boolean): T[] {
-            if (!compareFn) return Array.from(new Set(this))
-            const $: T[] = []
-            this.forEach(item => {
-                if ($.some(it => compareFn(item, it))) return
-                $.push(item)
-            })
-            return $
-        }
-        Array.prototype.toDeduplicated = toDeduplicated
-    }
-    if (!Array.prototype.hasOwnProperty("toSpliced")) {
-        const toSpliced = function <T>(this: T[], start: number, deleteCount?: number, ...items: T[]): T[] {
-            const $ = [...this]
-            if (deleteCount === undefined) {
-                $.splice(start)
-            } else {
-                $.splice(start, deleteCount, ...items)
-            }
-            return $
-        }
-        Array.prototype.toSpliced = toSpliced
-    }
-    if (!Array.prototype.hasOwnProperty("toPushed")) {
-        const toPushed = function <T>(this: T[], ...items: T[]): T[] {
-            const $ = [...this]
-            $.push(...items)
-            return $
-        }
-        Array.prototype.toPushed = toPushed
-    }
-    if (!Array.prototype.hasOwnProperty("toUnshifted")) {
-        const toUnshifted = function <T>(this: T[], ...items: T[]): T[] {
-            const $ = [...this]
-            $.unshift(...items)
-            return $
-        }
-        Array.prototype.toUnshifted = toUnshifted
-    }
-    if (!Array.prototype.hasOwnProperty("toExchange")) {
-        const toExchange = function <T>(this: T[], a: number, b: number): T[] {
-            return this.with(a, this[b]).with(b, this[a])
-        }
-        Array.prototype.toExchange = toExchange
-    }
-    if (!Array.prototype.hasOwnProperty("at")) {
-        const at = function <T>(this: T[], index: number): T | undefined {
-            if (!Number.isInteger(index)) {
-                throw new RangeError(`Invalid index : ${index}`)
-            }
-            return this[index >= 0 ? index : this.length + index]
-        }
-        Array.prototype.at = at
-    }
+    return resultList
+}
+
+export function lastTimeOfDay(time: number) {
+    return dayjs(dayjs(time).format("YYYY-MM-DD")).valueOf() + 24 * 60 * 60 * 1000 - 1
 }
